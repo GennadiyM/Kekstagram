5e211d13e5a0e58b319605058471171e4a28854c
Геннадий Максимов <mr.gennadiy.maksimov@gmail.com>
2019-01-15 19:50:29 +0300

    final check

--- js/scale-img.js
+++ js/scale-img.js
@@ -8,17 +8,17 @@
 
   window.scaleImg = {
     onChangeScaleBigger: function () {
-      var modifiedValueScale = parseInt(scaleControlValue.value, 10) + STEP_SCALE;
+      var modifiedValueScale = parseInt(controlValue.value, 10) + STEP_SCALE;
       modifiedValueScale = Math.min(window.uploadImg.Filters.SCALE.maxValueFilter, Math.max(modifiedValueScale, window.uploadImg.Filters.SCALE.minValueFilter));
-      scaleControlValue.value = modifiedValueScale + '%';
-      window.uploadImg.preview.style.transform = window.uploadImg.Filters.SCALE.cssFilter(parseInt(scaleControlValue.value, 10));
+      controlValue.value = modifiedValueScale + '%';
+      window.uploadImg.preview.style.transform = window.uploadImg.Filters.SCALE.cssFilter(parseInt(controlValue.value, 10));
       return window.uploadImg.preview;
     },
     onChangeScaleSmaller: function () {
-      var modifiedValueScale = parseInt(scaleControlValue.value, 10) - STEP_SCALE;
+      var modifiedValueScale = parseInt(controlValue.value, 10) - STEP_SCALE;
       modifiedValueScale = Math.min(window.uploadImg.Filters.SCALE.maxValueFilter, Math.max(modifiedValueScale, window.uploadImg.Filters.SCALE.minValueFilter));
-      scaleControlValue.value = modifiedValueScale + '%';
-      window.uploadImg.preview.style.transform = window.uploadImg.Filters.SCALE.cssFilter(parseInt(scaleControlValue.value, 10));
+      controlValue.value = modifiedValueScale + '%';
+      window.uploadImg.preview.style.transform = window.uploadImg.Filters.SCALE.cssFilter(parseInt(controlValue.value, 10));
       return window.uploadImg.preview;
     },
     controlValue: controlValue,
